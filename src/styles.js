2c64c7c91ce478dd4e7e6010089ca8e52f4530ca
jpabl@0s1n <jpablo_166@hotmail.com>
2023-10-03 14:28:18 -0500

    style(animations):styles.js

--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/prefer-default-export */
-export const { styles } = {
+export const styles = {
     paddingX: 'sm:px-16 px-6',
     paddingY: 'sm:py-16 py-6',
     padding: 'sm:px-16 px-6 sm:py-16 py-10',
