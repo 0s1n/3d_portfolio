632bdcabc6189a89be79a2a4db002e84f62af037
jpabl@0s1n <jpablo_166@hotmail.com>
2023-10-08 21:10:06 -0500

    feat(elements)created FeadBackSection

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { motion } from 'framer-motion';
 
-const Feedbacks = () => {
+import SectionWrapper from '../hoc';
+import { styles } from '../styles';
+import { textVariant } from '../utils/motion';
+import { testimonials } from '../constants';
+import FeedbackCard from './cards/FeedbackCard';
+
+function Feedbacks() {
   return (
-    <div>Feedbacks</div>
-  )
+    <div className="mt-12 bg-black-100 rounded-[20px]">
+      <div
+        className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}
+      >
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>What others say</p>
+          <h2 className={styles.sectionHeadText}>Testimonials.</h2>
+        </motion.div>
+      </div>
+
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard
+            key={testimonial.name}
+            index={index}
+            testimonial={testimonial.testimonial}
+            name={testimonial.name}
+            designation={testimonial.designation}
+            company={testimonial.company}
+            image={testimonial.image}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Feedbacks
+const FeedbacksSection = SectionWrapper(Feedbacks, '');
+export default FeedbacksSection;
