b2906692ee8576597c8fe66c2e8b2a0c1ad7e6b7
jpabl@0s1n <jpablo_166@hotmail.com>
2023-10-08 21:46:42 -0500

    feat(elements):created WorksSection

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { fadeIn, textVariant } from '../utils/motion';
+import { projects } from '../constants';
+import { ProjectCard } from './cards';
+import SectionWrapper from '../hoc';
 
-const Works = () => {
+function Works() {
   return (
-    <div>Works</div>
-  )
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText}`}>My Work</p>
+        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn('', '', 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          Following proyects showcases my skills and experience through
+          real-world examples of my work. Each proyect is briefly described with
+          links to code repositories and live demos in it. It reflects my
+          ability to solve complex problems, work with different technologies,
+          and manage proyects effectively.
+        </motion.p>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard
+            key={project.id}
+            index={index}
+            name={project.name}
+            description={project.description}
+            tags={project.tags}
+            image={project.image}
+            sourceCodeLink={project.source_code_link}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default Works
+const WorksSection = SectionWrapper(Works, '');
+export default WorksSection;
