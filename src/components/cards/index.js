141d277bbbc4fe5e5382ef009fb92f148e313af1
jpabl@0s1n <jpablo_166@hotmail.com>
2023-10-08 21:47:53 -0500

    perf(common):updated barrel

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,3 +1,4 @@
+export { default as ExperienceCard } from './ExperienceCard';
 export { default as FeedbackCard } from './FeedbackCard';
 export { default as ProjectCard } from './ProjectCard';
 export { default as ServiceCard } from './ServiceCard';
